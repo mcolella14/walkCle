c13807afd53d929e13df682ae7dcdb5137d4c390
James Carkido <mr.carkido@gmail.com>
2019-04-09 09:55:47 -0400

    .

--- FrontEnd/walk-cle/src/views/Login.vue
+++ FrontEnd/walk-cle/src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
 <div id="login">
     <h1>Login</h1>
-        <form>
+        <form @submit.prevent="login(this.user)">
             <input type="text" name="username" v-model="user.username" placeholder="Username" />
             <input type="password" name="password" v-model="user.password" placeholder="Password" />
-            <button type="button" v-on:click="login(this.user)">Login</button>
+            <button type="submit" >Login</button>  <!-- v-on:click="login(this.user)" -->
         </form>
 </div>
 </template>
@@ -12,24 +12,33 @@
 <script>
 export default {
     data(){
+        let isAuthorized = false;
         return {
             user:{
                 username: "",
                 password: ""
-            }
+            },
         }
     },
     methods: {
-        login(userInfo){
+        loginSuccess(response){
+            if(response.data.success){
+                this.isAuthorized = true;
+            }
+        },
+
+        login(){
+            console.log(this.user.username + this.user.password);
             fetch(`${this.API_URL}/account/register`,{
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(userInfo)
+            body: JSON.stringify(this.user)
             })
             .then((response) => {
                 if(response.ok){
+                    // $emit isAuthorized === true  ??
                     this.$router.push("/");
                 }
             })

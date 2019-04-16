247d68f50dae387f0780a8a9657087b933a84f3e
Matthew Colella <mcolella14@gmail.com>
2019-04-16 12:37:33 -0400

    added front end to display a list of checked in locations

--- FrontEnd/walk-cle/src/router.js
+++ FrontEnd/walk-cle/src/router.js
@@ -51,7 +51,7 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/', '/login', '/register','/addLocation'];
+  const publicPages = ['/', '/login', '/register','/addLocation', 'profile'];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = auth.getUser();
 
